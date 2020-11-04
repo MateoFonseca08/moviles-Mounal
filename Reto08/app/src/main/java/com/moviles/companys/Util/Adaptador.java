a4d984003bc9bad3113ebb5ff648aae9324607b1
MateoFonseca08 <71329350+MateoFonseca08@users.noreply.github.com>
2020-11-03 19:14:48 -0500

    Reto 08
    Reto08

---
+++ Reto08/app/src/main/java/com/moviles/companys/Util/Adaptador.java
@@ -0,0 +1,60 @@
+package com.moviles.companys.Util;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.moviles.companys.R;
+
+import java.util.ArrayList;
+
+public class Adaptador extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+
+    Context contexto;
+    ArrayList<ArrayList<String>> datos;
+
+    public Adaptador(Context contexto, ArrayList<ArrayList<String>> datos)
+    {
+        this.contexto = contexto;
+        this.datos = datos;
+        inflater = (LayoutInflater)contexto.getSystemService(contexto.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        final View vista = inflater.inflate(R.layout.item_company, null);
+        TextView game_name = (TextView) vista.findViewById(R.id.name_company);
+        TextView classification = (TextView) vista.findViewById(R.id.clasification);
+        ImageView image = (ImageView) vista.findViewById(R.id.image);
+        game_name.setText(datos.get(i).get(1));
+        classification.setText("Classification: "+datos.get(i).get(3));
+        int type = Integer.parseInt(datos.get(i).get(2));
+        switch (type){
+
+        }
+        return vista;
+    }
+
+    @Override
+    public int getCount() {
+        return datos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+}
---
+++ Reto08/app/src/main/java/com/moviles/companys/Util/Constanst.java
@@ -0,0 +1,10 @@
+package com.moviles.companys.Util;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class Constanst {
+    public static SQLiteDatabase db;
+    public static Company company = new Company();
+    public static String search_name;
+    public static int search_classification;
+}
